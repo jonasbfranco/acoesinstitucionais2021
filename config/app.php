b55ffb52f8348a04629a1100c50daf24d40ea009
jonasbfranco <jonasbfranco@hotmail.com>
2021-03-10 20:06:59 -0300

    teste

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://www2.usinacolombo.com.br/ai2021/'),
 
     'asset_url' => env('ASSET_URL', null),
 
